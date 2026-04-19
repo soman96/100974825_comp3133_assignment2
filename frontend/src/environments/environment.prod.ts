11618a78dff323ebdcf2e8a93090b9c3b2a5f76f
soman96 <ahmad@soman.ca>
2026-04-19 17:56:10 -0400

    fixed backend uri

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ng build --configuration production will use this file
-  graphqlUri: 'https://100974825-comp3133-assignment2.vercel.app',
+  graphqlUri: 'https://100974825-comp3133-assignment2.vercel.app/graphql',
 };
